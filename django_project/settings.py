61e3960a81819aa6f92aaee0562774191d7f43fe
deepaksemwal121 <deepaksemwal121@gmail.com>
2022-12-04 22:02:12 +0530

    Added API App

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd Party
+    'rest_framework',
     # New App 
     "books.apps.BooksConfig",
+    'apis.apps.ApisConfig',
 ]
 
 MIDDLEWARE = [
